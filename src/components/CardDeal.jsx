1e12864315df337fc8f60becbc1af5ca5bd0c2c5
mapper895 <mapper.895.mp@gmail.com>
2022-09-13 14:31:36 -0500

    Card Deal

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+import { card } from "../assets";
+import styles, { layout } from "../styles";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few ease
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil quod
+        veritatis eveniet maxime vero amet neque vel, ab dignissimos provident.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
 
 export default CardDeal;
